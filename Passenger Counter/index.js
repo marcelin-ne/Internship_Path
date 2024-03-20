3669f717d07282a2ed7484d969468f561e803274
Marcelinne <marcela.montalvo@epn.edu.ec>
2024-03-20 12:44:25 -0500

    feat: Implement JavaScript functionality for incrementing count and saving entries
    This commit includes the following changes:
    
    - Initialize count variable to 0
    - Retrieve elements by their IDs: count-el and save-el
    - Implement increment function to increase count when button is clicked
    - Display updated count on the webpage
    - Implement save function to store count and add it to the previous entries
    - Reset count to 0 after saving
    - Log count and elements to console for debugging purposes

--- Passenger Counter/index.js
+++ Passenger Counter/index.js
@@ -1,1 +1,45 @@
-document.getElementById("count-el").innerText = 5;
+// document.getElementById("count-el").innerText = 5;
+
+/*Lesson 2: Write your first JavaScript variable */
+
+// let count = 5
+
+/* Reassigning and incrementing */
+
+// count = count + 1
+
+
+/*Lesson 3: The onclick event listener*/
+
+let count=0
+let countEl = document.getElementById("count-el")
+let saveEl= document.getElementById("save-el")
+
+console.log(countEl) // Return a representation js
+console.log(saveEl)
+// Listen for clicks on the increment button
+
+//Increment the count variable when the button is clicked
+
+function increment() {
+    console.log("The button was clicked")
+    count += 1
+    countEl.textContent= count
+    console.log(count)
+}
+
+/*Lesson 4: Add a save buttom and function*/
+function save() {
+    let countStr = count + " - "
+
+/* Lesson 5: Add a string Previus entries */
+    saveEl.textContent += countStr
+
+/* Lesson 6: Reset the count */
+    countEl.textContent = 0
+    count = 0
+    console.log(count)
+}
+
+/* Alternatives to innerText */
+// TextContent

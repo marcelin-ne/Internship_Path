df9236ecf2f3312c1db44166fe9313cba6f2cbf4
Marcelinne <marcela.montalvo@epn.educe>
2024-04-16 11:26:24 -0500

    refactor (githubApi) Change class to just functions

--- GithubRepository/src/githubApi.js
+++ GithubRepository/src/githubApi.js
@@ -1,44 +1,44 @@
 const fs = require('fs');
-class GitHubClient {
-    constructor() {
-        this.token = process.env.GITHUB_TOKEN;;
-        this.baseUrl = 'https://api.github.com';
+
+const token = process.env.GITHUB_TOKEN;
+const baseUrl= 'https://api.github.com';
+const org_Name = 'stackbuilders';
+const baseUrlOrg = `https://api.github.com/orgs/${org_Name}/repos`;
+
+    async function testConnection(token) {
+    if (!token) {
+        console.error('No se ha proporcionado un token de acceso. Configure la variable de entorno GITHUB_TOKEN.');
+        return false;
     }
 
-    async testConnection() {
-        if (!this.token) {
-            console.error('No se ha proporcionado un token de acceso. Configure la variable de entorno GITHUB_TOKEN.');
-            return false;
-        }
-
-        try {
-            const response = await fetch(`${this.baseUrl}/user`, {
-                headers: {
-                    Authorization: `token ${this.token}`
-                }
-            });
-
-            if (response.ok) {
-                const userData = await response.json();
-                console.log('Conexión exitosa. Bienvenido, ' + userData.login);
-                return true;
-            } else {
-                console.error('Error de conexión:', response.statusText);
-                return false;
+    try {
+        const response = await fetch(`${baseUrl}/user`, {
+            headers: {
+                Authorization: `token ${token}`
             }
-        } catch (error) {
-            console.error('Error de conexión:', error.message);
+        });
+
+        if (response.ok) {
+            const userData = await response.json();
+            console.log('Conexión exitosa. Bienvenido, ' + userData.login);
+            return true;
+        } else {
+            console.error('Error de conexión:', response.statusText);
             return false;
         }
+    } catch (error) {
+        console.error('Error de conexión:', error.message);
+        return false;
     }
+}
 
 //Create get Repositories info by organization
 
-    async getRepositories(orgName , baseUrl){
+    async  function getRepositories(org_Name , baseUrlOrg){
         try{
-            const response= await fetch (baseUrl, {
+            const response= await fetch (baseUrlOrg, {
                 header: {
-                    Authorization: 'token ${this.authToken}',
+                    Authorization: 'token ${authToken}',
                 },
             });
             return await response.json();
@@ -49,7 +49,7 @@ class GitHubClient {
     }
 
 
-    saveJsonToFile(data, filename) {
+    function saveJsonToFile(data, filename) {
         const jsonData = JSON.stringify(data, null, 2); // Formatear con sangría de 2 espacios
         fs.writeFileSync(filename, jsonData);
         console.log(`JSON data saved to ${filename}`);
@@ -57,21 +57,12 @@ class GitHubClient {
 
 
 
-}
-
-githubClient = new GitHubClient();
-githubClient.testConnection()
-
-const orgName = 'stackbuilders';
-const baseUrl = `https://api.github.com/orgs/${orgName}/repos`;
-
-githubClient.getRepositories(orgName,baseUrl)
-    .then (repositories => {
-        if (repositories){
-            console.log('Repositories:');
-            githubClient.saveJsonToFile(repositories,'repositories.json');
+    testConnection(baseUrl, token)
+    .then(connectionSuccessful => {
+        if (connectionSuccessful) {
+            console.log('La conexión fue exitosa.');
+        } else {
+            console.log('La conexión falló.');
         }
-    })
-    .catch(error => console.error('Error:', error));
-
+    });
 
